6f6637327b87fafac6f731b4923b9f6bc124fb4e
Patrick McCombs <pa.mccombs1@gmail.com>
2018-01-01 15:32:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ function happyHolidaysTo(name)  {
 }
 
 function happyHolidayTo(holiday, name) {
-  return "Happy '${holiday}', '${name}!'"
+  return "Happy Independence Day, you!"
 }
 
 function holidayCountdown(holiday, days) {
-  return "It\'s '${days}' days until '${holiday}'!"
+  return "It\'s 20ays until '${holiday}'!"
 }
