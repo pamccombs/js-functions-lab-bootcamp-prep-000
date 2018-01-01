04184c603065ae38e087c25ec28f338b7ae86f28
Patrick McCombs <pa.mccombs1@gmail.com>
2018-01-01 16:31:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function happyHolidaysTo(name)  {
 }
 
 function happyHolidayTo(holiday, name) {
-  return "Happy Independence Day, you"
+  return "Happy Independence Day, you!"
 }
 
 function holidayCountdown(holiday, days) {
